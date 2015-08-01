d607bae47fcbcbeb9f6da41831f6ada6f39f6471
valepert <valerio@tylton.local>
2015-08-01 20:14:38 +0200

    gexf & continue
    Loop and saving graphs

--- network.py
+++ network.py
@@ -4,14 +4,18 @@ from glob import glob
 import json
 import networkx as nx
 
+namespaces = {'it': 'Utente:', 'en': 'User:', 'de': 'Benutzer:'}
+
 project = 'it'
-unamespace = 'Utente:'
+unamespace = namespaces[project]
 thanksdir = 'thanks_%s' % project
 
 thks = []
 for filename in glob('%s/*' % thanksdir):
     with open(filename) as fp:
         thks += [json.load(fp)]
+
+print(len(thks))
 
 thankers = set(tnk['user'] for tnk in thks)
 thanked = set(thk['title'].lstrip(unamespace) for thk in thks)
@@ -21,7 +25,19 @@ edges = [(thk['user'], thk['title'].lstrip(unamespace),
           {'ts': thk['timestamp']})
           for thk in thks]
 
+print(len(users), len(edges))
+
 G = nx.DiGraph()
 G.add_nodes_from(users)
 G.add_edges_from(edges)
+nx.write_gexf(G, '%s_digraph.gexf' % (thanksdir))
+print (G.number_of_nodes(), G.number_of_edges())
+
+U = G.to_undirected()
+nx.write_gexf(U, '%s_graph.gexf' % (thanksdir))
+print (U.number_of_nodes(), U.number_of_edges())
+
+giant = max(nx.connected_component_subgraphs(U), key=len)
+nx.write_gexf(giant, '%s_giant.gexf' % (thanksdir))
+print (giant.number_of_nodes(), giant.number_of_edges())
 
--- thank.py
+++ thank.py
@@ -17,15 +17,38 @@ project = 'it'
 api = 'w/api.php'
 url = 'http://%s.wikipedia.org/%s' %  (project, api)
 thanksdir = 'thanks_%s' % project
+lelimit = 500
+n_times = 3
+sleep_time = 60
 
 wiki = simplemediawiki.MediaWiki(url)
 simplemediawiki.build_user_agent(**user_agent)
 
 #lastcontinue = u''
 query = {'action': 'query', 'list': 'logevents', 'letype': 'thanks',
+         'lelimit': lelimit,
          'continue': u''} # u'-||' } #, 'lecontinue': lastcontinue}
 
 out = wiki.call(query)
 results = out['query']['logevents']
 dump_results(results, thanksdir)
+cont = out['continue']
 
+while results:
+    for _ in range(n_times):
+        try:
+            query = dict(query, **cont)
+            out = wiki.call(query)
+            results = out['query']['logevents']
+            dump_results(results, thanksdir)
+            print(cont['lecontinue'], len(results))
+            cont = out['continue']
+            time.sleep(sleep_time)
+            break
+        
+        except Exception as e:
+            print("Raised exception: %s" % e)
+            time.sleep(sleep_time * 5)
+            pass
+    else:
+        raise
